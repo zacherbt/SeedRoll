a2fa3a7bd9119e5757e06ed1f2040a91a2bea25d
shakybake <116125215+shakybake@users.noreply.github.com>
2022-10-19 13:16:25 -0700

    Add files via upload
    Remember to add a .env file with the bot token to successfully access login.

--- SeedRoll/SeedRollBot/src/main/java/SeedRoll.java
+++ SeedRoll/SeedRollBot/src/main/java/SeedRoll.java
@@ -1,18 +1,45 @@
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
 
 import javax.security.auth.login.LoginException;
 
 public class SeedRoll
 {
-    public static void main(String[] args) throws LoginException
+    private final ShardManager shardManager;
+    private final Dotenv config;
+
+
+
+    public SeedRoll() throws LoginException
     {
-        // replace token with better implementation outside of source code
-        final String TOKEN = "Token";
-        JDABuilder jdaBuilder = JDABuilder.createDefault(TOKEN);
+        config = Dotenv.configure().ignoreIfMissing().load();
+        String token = config.get("TOKEN");
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        builder.setActivity(Activity.watching("for Rollplayers"));
+        shardManager = builder.build();
+    }
 
-        jdaBuilder.build();
+    public Dotenv getConfig()
+    {
+        return config;
+    }
 
+    public ShardManager getShardManager()
+    {
+        return shardManager;
+    }
 
-
+    public static void main(String[] args)
+    {
+        try
+        {
+            SeedRoll seedRoll = new SeedRoll();
+        } catch (LoginException e)
+        {
+            System.out.println("ERROR: Invalid Bot token.");
+        }
     }
 }
